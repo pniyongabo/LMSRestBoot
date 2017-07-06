0e797fa4e570984ffc1a864f86112f643bf5cc3a
npatrick96 <patrickniyongabo@gmail.com>
2017-07-06 16:37:26 -0400

    added actuator dependency and removed mongoDB

--- src/test/java/com/gcit/lms/test/LMSConfig.java
+++ src/test/java/com/gcit/lms/test/LMSConfig.java
@@ -1,12 +1,9 @@
 package com.gcit.lms.test;
 
-import java.net.UnknownHostException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -14,7 +11,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 import com.gcit.lms.dao.AuthorDAO;
 import com.gcit.lms.dao.BookDAO;
 import com.gcit.lms.service.AdminService;
-import com.mongodb.MongoClient;
 
 @Configuration
 public class LMSConfig {
@@ -58,15 +54,5 @@ public class LMSConfig {
 	@Bean
 	public PlatformTransactionManager txManager(){
 		return new DataSourceTransactionManager(dataSource());
-	}
-	
-	@Bean
-	public SimpleMongoDbFactory mongoDbFactory() throws UnknownHostException{
-		return new SimpleMongoDbFactory(new MongoClient(), "local");
-	}
-	
-	@Bean
-	public MongoTemplate mongoTemplate() throws UnknownHostException{
-		return new MongoTemplate(mongoDbFactory());
 	}
 }
