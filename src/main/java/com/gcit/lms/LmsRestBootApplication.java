f8574219ac90f3fafb442a17be5835cc699d8890
npatrick96 <patrickniyongabo@gmail.com>
2017-07-06 16:30:28 -0400

    readded the Application main class that I have accidently deleted

---
+++ src/main/java/com/gcit/lms/LmsRestBootApplication.java
@@ -0,0 +1,12 @@
+package com.gcit.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsRestBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsRestBootApplication.class, args);
+	}
+}
