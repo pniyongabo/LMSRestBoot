2e72f033d14f122c06ad6f0f766da0eb9987e95a
npatrick96 <patrickniyongabo@gmail.com>
2017-07-28 13:40:40 -0400

    back to using the AWS RDS as DB

--- src/main/java/com/gcit/lms/LMSConfig.java
+++ src/main/java/com/gcit/lms/LMSConfig.java
@@ -30,17 +30,17 @@ public class LMSConfig extends WebMvcConfigurerAdapter{
 	
 	static Logger logger  = LoggerFactory.getLogger(LMSConfig.class);
 	
-	public String driver = "com.mysql.jdbc.Driver";
-	public String url = "jdbc:mysql://localhost/library";
-	public String username = "root";
-	public String password = "root";
-	
 //	public String driver = "com.mysql.jdbc.Driver";
-//	public String awsDbEndpoint = "testrds.c4uwpmj5ivko.us-east-1.rds.amazonaws.com:3306/";
-//	public String dbName = "library";
-//	public String url = "jdbc:mysql://" + awsDbEndpoint + dbName;
-//	public String username = "rootroot";
-//	public String password = "rootroot";
+//	public String url = "jdbc:mysql://localhost/library";
+//	public String username = "root";
+//	public String password = "root";
+	
+	public String driver = "com.mysql.jdbc.Driver";
+	public String awsDbEndpoint = "testrds.c4uwpmj5ivko.us-east-1.rds.amazonaws.com:3306/";
+	public String dbName = "library";
+	public String url = "jdbc:mysql://" + awsDbEndpoint + dbName;
+	public String username = "rootroot";
+	public String password = "rootroot";
 	
 	@Value("${spring.profiles.active}")
 	private String profile;
